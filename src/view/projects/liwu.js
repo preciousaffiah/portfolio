3ffd0bdbfeaef15f9b3de14be50638fa3cdc067a
Precious Affiah <74688829+preciousaffiah@users.noreply.github.com>
2025-04-13 01:36:08 +0100

    Update liwu.js

--- src/view/projects/liwu.js
+++ src/view/projects/liwu.js
@@ -9,11 +9,11 @@ const Liwu = () => {
       <Header page="Liwu Airdrop" link="/projects/liwu" />
       <Project
         page="Liwu Airdrop"
-        description='LiWU Panda is a creative symbol of play, connection, and community in the blockchain space, inspired by the Chinese word for “gift” (禮), and represents a movement of generosity.'
+        description='Liwu - The AI-Powered Gift Token for Gamers, Streamers and Social Connections Built on BNB.'
         link="https://airdrops.liwupanda.com"
         year="2024"
         role="Frontend"
-        overview="Liwu - The AI-Powered Gift Token for Gamers, Streamers and Social Connections Built on BNB"
+        overview="LiWU Panda is a creative symbol of play, connection, and community in the blockchain space, inspired by the Chinese word for “gift” (禮), and represents a movement of generosity."
         image={liwu}
       />
     </div>
