4f7beae94c418c0ae497c131bd4d62b885407f56
Precious Affiah <74688829+preciousaffiah@users.noreply.github.com>
2025-06-18 21:04:11 +0100

    Update home.js

--- src/view/home.js
+++ src/view/home.js
@@ -224,25 +224,6 @@ const HomePage = () => {
                   </PageAnimation>
                 </div>
               </div>
-              <Link to="/projects">
-                <div className="flex pt-5 justify-center font-semibold">
-                  <p>see more</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="lucide lucide-chevron-down"
-                  >
-                    <path d="m6 9 6 6 6-6" />
-                  </svg>
-                </div>
-              </Link>
             </div>
           </div>
           <div className="section-3 w-full">
